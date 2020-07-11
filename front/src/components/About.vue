d16f182038fc7d2cf656a0457868fec21111e8c6
Thomas-Chiu <josemich0511@gmail.com>
2020-07-11 12:30:30 +0800

    fix carousel text height

--- front/src/components/About.vue
+++ front/src/components/About.vue
@@ -1,23 +1,26 @@
 <template lang="pug">
-  .container
+  .container-fluid
     .row
       .col-12
         h2 ABOUT
-      .col-12
-        .carousel-text
-          b-carousel#carousel-fade(
-            fade
+      .col-12.carousel-text
+        b-carousel#carousel-fade(fade)
+          b-carousel-slide(
+            :interval="3000"
+            img-width="0"
+            img-height="0"
+            caption="19101 cuisine"
+            img-blank
             )
-            b-carousel-slide(
-              caption="19101 cuisine"
-              img-blank
-              )
-              p 健康是一生的事情，<br>吃飯是天天的事情。
-            b-carousel-slide(
-              caption='關於啖飯'
-              img-blank
-              )
-              p 猴頭菇跟臭豆腐是招牌，然後<br>剛好有不少的食材是有機的。
+            p 健康是一生的事情，<br>吃飯是天天的事情。
+          b-carousel-slide(
+            :interval="3000"
+            img-width="0"
+            img-height="0"
+            caption='關於啖飯'
+            img-blank
+            )
+            p 猴頭菇跟臭豆腐是招牌，然後<br>剛好有不少的食材是有機的。
 
 </template>
 
--- front/src/components/Event.vue
+++ front/src/components/Event.vue
@@ -3,21 +3,26 @@
     h2 EVENTS
     b-card(no-body)
       b-row(no-gutters)
-        b-col(md='4')
+        b-col(md="4")
           b-card-body(title="News")
             b-tabs(pills vertical)
-              b-tab(title='Tab 1' active)
-                b-card-text Tab contents 1
-              b-tab(title='Tab 2')
-                b-card-text Tab contents 2
-              b-tab(title='Tab 3')
-                b-card-text Tab contents 3
-        b-col(md='4')
-          b-card-img(src='https://picsum.photos/800/600/?image=20' alt='Image')
-        b-col(md='4')
-          b-card-body(title='Details')
+              b-tab(title="活動一" active)
+                b-card-text 內容一
+              b-tab(title="活動二")
+                b-card-text 內容二
+              b-tab(title="活動三")
+                b-card-text 內容三
+        b-col(md="4")
+          b-card-img(src="https://picsum.photos/400/400/?random=1" alt='活動照片')
+        b-col(md="4")
+          b-card-body(title="Details")
             b-card-text
-              | This is a wider card with supporting text as a natural lead-in to additional content. This content is a little bit longer.
+              | 這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案這邊放活動內容的文案
+      b-row.btn-function(no-gutters)
+        b-col(md="6")
+          b-button(block) BOOK A TABLE
+        b-col(md="6")
+          b-button(block) ORDER A DISH
 
 </template>
 
