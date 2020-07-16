80793582648d4f1b9d8b09f87951cfe2838068f7
Thomas-Chiu <josemich0511@gmail.com>
2020-07-16 22:56:50 +0800

    img path unsolved

--- front/src/components/Overview.vue
+++ front/src/components/Overview.vue
@@ -16,6 +16,8 @@
         </template>
         <template>
           <img src="https://scontent.ftpe7-4.fna.fbcdn.net/v/t1.0-9/p960x960/97053182_705931250152240_7873890409053683712_o.jpg?_nc_cat=107&_nc_sid=8024bb&_nc_oc=AQlefAOmNSO0ppx8qPyXrUXMTLAk0fLGGcbES4NTc9xQISj5a40-8TTel7_jLYATBlE&_nc_ht=scontent.ftpe7-4.fna&_nc_tp=6&oh=54db421725a30abc905392db3cb35dab&oe=5F31A623" alt="餐點照片">
+          <!-- img 相對路徑 build 出來有問題 -->
+          <img src="../../../materials/menu/乾麵.png" alt="">
         </template>
         <template v-slot:footer="slotProps">
           <strong>Id: {{ slotProps.footerProp.id }}</strong>
