914c204e8d42c053774034aea72a1a4371e051e3
Thomas-Chiu <josemich0511@gmail.com>
2020-07-20 16:36:35 +0800

    start from API

---
+++ back/db.js
@@ -0,0 +1,116 @@
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import beautifyUnique from 'mongoose-beautiful-unique-validation'
+import validator from 'validator' // 驗證套件
+
+dotenv.config()
+
+const Schema = mongoose.Schema
+mongoose.connect(process.env.DBURL)
+mongoose.plugin(beautifyUnique)
+
+const userSchema = new Schema(
+  {
+    account: {
+      type: String,
+      minlength: [4, '帳號必須四個字以上'],
+      maxlength: [20, '帳號不超過二十字'],
+      unique: '帳號已使用',
+      required: [true, '請輸入帳號']
+    },
+    password: {
+      type: String,
+      required: [true, '請輸入密碼']
+    },
+    email: {
+      type: String,
+      required: [true, '請輸入信箱'],
+      validate: {
+        validator (value) {
+          return validator.isEmail(value)
+        },
+        message: '信箱格式錯誤'
+      }
+    }
+  }, {
+    versionKey: false
+  }
+)
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '請輸入菜名']
+    },
+    amount: {
+      type: Number,
+      required: [true, '請輸入數量']
+    },
+    price: {
+      type: Number,
+      required: [true, '請輸入價錢']
+    },
+    description: {
+      type: String,
+      maxlength: [200, '圖文說明 200 字以下']
+    },
+    sauce: {
+      type: String
+    },
+    drink: {
+      type: String
+    },
+    mainCourse: {
+      type: String
+    },
+    noodle: {
+      type: String
+    },
+    img: {
+      type: String
+    }
+  }, {
+    versionKey: false
+  }
+)
+
+const orderSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, '請輸入訂購人姓名']
+    },
+    gender: {
+      type: Boolean
+    },
+    mobile: {
+      type: Number,
+      required: [true, '請輸入電話號碼']
+    },
+    here: {
+      type: Boolean,
+      required: [true, '請選擇內用或外帶']
+    },
+    pickupTime: {
+      type: String
+    },
+    memo: {
+      type: String
+    }
+  }, {
+    versionKey: false
+  }
+)
+
+const users = mongoose.model(process.env.COLLECTION_USERS, userSchema)
+const products = mongoose.model(process.env.COLLECTION_PRODUCTS, productSchema)
+const orders = mongoose.model(process.env.COLLECTION_ORDERS, orderSchema)
+const connection = mongoose.connection
+
+export default {
+  users,
+  products,
+  orders,
+  connection
+}
---
+++ back/index.js
@@ -0,0 +1,63 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+// import multer from 'multer' // 處理上傳的檔案
+// import FTPStorage from 'multer-ftp' // 將上傳的檔案丟到FTP
+// import md5 from 'md5'
+// import path from 'path' // path 為node.js 內建套件，可處理路徑資訊
+// import fs from 'fs' // fs 為node.js 內建套件，可處理檔案系統
+import dotenv from 'dotenv'
+import connectMongo from 'connect-mongo'
+import session from 'express-session'
+
+import db from './db.js'
+
+dotenv.config()
+
+// 套件設定區 ---------------------------------------------------------------------------
+const MongoStore = connectMongo(session)
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cors({ // cors 跨網域設定
+  origin (origin, callback) { // 直接開網頁，不是ajax 時，origin 是undefined
+    if (origin === undefined) {
+      callback(null, true)
+    } else {
+      if (process.env.ALLOW_CORS === 'true') { // 上傳FTP 時，dot.env 不知為何將bool 視為字串，因此判斷式的bool 改成字串
+        callback(null, true) // 若開發環境，允許
+      } else if (origin.includes('github')) {
+        callback(null, true) // 非開發環境，但從github 過來，允許
+      } else {
+        callback(new Error('Not Allowed'), false) // 非開發環境也非github 過來，拒絕
+      }
+    }
+  },
+  credentials: true
+}))
+
+app.use(session({ // express-session 設定
+  secret: 'album',
+  store: new MongoStore({ // 將session 存進mongoDB
+    mongooseConnection: db.connection, // 使用mongoose 的資料庫連接
+    collection: process.env.COLLECTION_SESSION // 設定存入的collection
+  }),
+  cookie: { // 設定session 有效期間
+    maxAge: 1000 * 60 * 30 // 1000ms * 60s * 30m = 30分鐘
+  },
+  saveUninitialized: false, // 是否保存未修改的session
+  rolling: true, // 是否每次重設過期時間
+  resave: true
+}))
+
+// 監聽server 區 ---------------------------------------------------------------------------
+app.listen(process.env.PORT, () => {
+  console.log('http://localhost:3000')
+  console.log('SERVER IS ON!!!')
+})
+
+// API 區 --------------------------------------------------------------------------------
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
